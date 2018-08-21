a3770ef4216e4569dc3bbfddeea30d260ba29e38
swFang <33407178+swFang@users.noreply.github.com>
2018-08-20 17:17:04 -0700

    finished comments blog feature

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -1,4 +1,8 @@
-class Article < ApplicationRecord
+#class Article < ApplicationRecord
+class Article < ActiveRecord::Base
+        has_many :comments
+    
+
     def find
         @Article = Article.find(params[:id])
     end
